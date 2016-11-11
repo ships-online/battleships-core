29375d90297448e11306a0b6404b65c8b342e1f3
Oskar Wrobel <kontakt@oskarwrobel.pl>
2016-11-11 15:26:29 +0100

    Improved tests and linting.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,29 @@
 'use strict';
 
 const config = {
-	ROOT_PATH: '.'
+	ROOT_PATH: __dirname
 };
 
+const path = require( 'path' );
 const gulp = require( 'gulp' );
+const del = require( 'del' );
+const utils = require( 'battle-ships-engine/dev/utils.js' )( config );
 const lintTasks = require( 'battle-ships-engine/dev/tasks/lint.js' )( config );
+const testTasks = require( 'battle-ships-engine/dev/tasks/test.js' )( config );
 const engineCompileTasks = require( 'battle-ships-engine/dev/tasks/compile.js' )( config );
 
+const options = utils.parseArgs( process.argv.slice( 3 ) );
+
 // Compile engine to esnext format.
-gulp.task( 'compile:engine', () => engineCompileTasks.compile( './engine', 'cjs' ) );
+gulp.task( 'clean:compile', () => del( './engine' ) );
+gulp.task( 'compile:engine', [ 'clean:compile' ], () => {
+	engineCompileTasks.compile( '../battle-ships-engine/src', './engine' )
+} );
 
 // JS code sniffer.
-gulp.task( 'lint', () => lintTasks.lint( '**/*.js' ) );
-gulp.task( 'pre-commit', () => lintTasks.lintStaged( '**/*.js' ) );
+const jsFiles = [ path.join( config.ROOT_PATH, '**', '*.js' ) ];
+
+gulp.task( 'lint', () => lintTasks.lint( jsFiles ) );
+gulp.task( 'pre-commit', () => lintTasks.lintStaged( jsFiles ) );
+
+gulp.task( 'test', ( done ) => testTasks.test( options, done ) );
