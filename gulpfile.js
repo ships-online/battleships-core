1f9af2db707d1f867b700ed04f92048731abe492
Oskar Wróbel <kontakt@oskarwrobel.pl>
2017-01-14 20:04:35 +0100

    Removed compilation dev task.

--- gulpfile.js
+++ gulpfile.js
@@ -8,20 +8,13 @@ const path = require( 'path' );
 const gulp = require( 'gulp' );
 
 const utils = require( 'battleships-dev-tools/lib/utils.js' );
-const linkTasks = require( 'battleships-dev-tools/lib/tasks/relink.js' )( config );
+const relinkTasks = require( 'battleships-dev-tools/lib/tasks/relink.js' )( config );
 const lintTasks = require( 'battleships-dev-tools/lib/tasks/lint.js' )( config );
 const testTasks = require( 'battleships-dev-tools/lib/tasks/test.js' )( config );
-const compileTasks = require( 'battleships-dev-tools/lib/tasks/compile.js' )( config );
 
 const options = utils.parseArgs( process.argv.slice( 3 ) );
 
-gulp.task( 'relink', linkTasks.relink );
-
-// Build game dependency.
-gulp.task( 'build:engine', () => compileTasks.buildDependency( 'engine' ) );
-gulp.task( 'build:utils', () => compileTasks.buildDependency( 'utils' ) );
-gulp.task( 'build:ui', () => compileTasks.buildDependency( 'ui-vanilla' ) );
-gulp.task( 'build', [ 'build:engine', 'build:utils', 'build:ui' ] );
+gulp.task( 'relink', relinkTasks.relink );
 
 // JS code sniffer.
 const jsFiles = [ path.join( config.ROOT_PATH, '**', '*.js' ) ];
