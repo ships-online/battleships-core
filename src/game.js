4c394bd87cddf7882110931867eb19831b718903
Oskar Wróbel <kontakt@oskarwrobel.pl>
2017-01-22 02:14:27 +0100

    Refactored Game class.

--- src/game.js
+++ src/game.js
@@ -1,6 +1,7 @@
 import Server from './server.js';
+import Player from './player.js';
 import PlayerBattlefield from 'battleships-engine/src/playerbattlefield.js';
-import AiBattlefield from 'battleships-engine/src/aibattlefield.js';
+import OpponentBattlefield from 'battleships-engine/src/opponentbattlefield.js';
 import GameView from 'battleships-ui-vanilla/src/gameview.js';
 import ObservableMixin from 'battleships-utils/src/observablemixin.js';
 import mix from 'battleships-utils/src/mix.js';
@@ -16,70 +17,93 @@ export default class Game {
 	 * @param {Object} [shipsConfig={ 1: 4, 2: 3, 3: 2, 4: 1 }] Defines how many ships of specified length will be in the game.
 	 */
 	constructor( server, size = 10, shipsConfig = { 1: 4, 2: 3, 3: 2, 4: 1 } ) {
-		/**
-		 * Size of the battlefield.
-		 *
-		 * @member {Number} game.Game#size
-		 */
-		this.size = size;
+		this.gameData = { size, shipsConfig };
 
 		this.set( 'gameId' );
 
-		/**
-		 * @type {game.Battlefield}
-		 */
-		this.playerBattlefield = new PlayerBattlefield( size, shipsConfig );
+		this.set( 'interestedPlayers', 0 );
 
-		/**
-		 * @type {game.Battlefield}
-		 */
-		this.opponentBattlefield = new AiBattlefield( size, shipsConfig );
+		this.set( 'isStarted', false );
+
+		this.size = size;
+
+		this.player = new Player( {
+			battlefield: new PlayerBattlefield( size, shipsConfig )
+		} );
+
+		this.opponent = new Player( {
+			battlefield: new OpponentBattlefield( size, shipsConfig )
+		} );
 
 		this.view = new GameView( this );
 
-		/**
-		 * Instance of a class for web sockets communication.
-		 *
-		 * @private
-		 * @type {game.Server}
-		 */
 		this._server = server;
 	}
 
-	/**
-	 * Send to the server configuration of player ships placement and inform opponent that player is ready for the battle.
-	 */
+	get isHost() {
+		return this.player.isHost;
+	}
+
+	get inviteUrl() {
+		return document.URL + '#' + this.gameId;
+	}
+
+	accept() {
+		if ( this.player.isPresent ) {
+			throw new Error( 'Game is full.' );
+		}
+
+		this._server.request( 'accept' )
+			.then( () => this.player.isPresent = true )
+			.catch( ( error ) => alert( error ) );
+	}
+
 	ready() {
-		if ( this.playerBattlefield.getWithCollision().length ) {
+		if ( this.player.isReady ) {
+			throw new Error( 'You are ready already.' );
+		}
+
+		if ( !this.player.isPresent ) {
+			throw new Error( 'You need to join the game first.' );
+		}
+
+		const ships = this.player.battlefield.shipsCollection.toJSON();
+
+		if ( !this.player.battlefield.validateShips( ships ) ) {
 			throw new Error( 'Invalid ships configuration.' );
 		}
 
-		this._server.emit( 'playerReady', { shipsDada: this.playerBattlefield.toJSON() } );
-	}
+		this.player.isReady = true;
 
-	/**
-	 * Send information to the opponent that player wants a rematch. If opponent will also request for a rematch then
-	 * rematch process.
-	 */
-	requestRematch() {
-		this._server.emit( 'rematch' );
+		this._server.request( 'ready', ships )
+			.catch( ( error ) => {
+				this.player.isReady = false;
+				alert( error );
+			} );
 	}
-
-	/**
-	 * Close connection with the server and release memory assets.
-	 */
-	destroy() {}
 
 	static create( element, size, shipsConfig ) {
 		return new Promise( ( resolve ) => {
 			const server = new Server();
 			const game = new Game( server, size, shipsConfig );
 
-			game.playerBattlefield.random();
-			element.appendChild( game.view.render() );
-			server.create( game.size, game.playerBattlefield.shipsCollection.shipsConfig )
-				.then( ( gameId ) => game.gameId = gameId );
+			game.player.battlefield.random();
+			game.player.isPresent = true;
+			game.player.isHost = true;
 
+			game._attachEvents();
+			server.on( 'accepted', () => game.opponent.isPresent = true );
+			server.on( 'left', ( event, data ) => {
+				if ( game.opponent.isReady || game.opponent.isPresent ) {
+					game.opponent.isReady = false;
+					game.opponent.isPresent = false;
+				}
+
+				game.interestedPlayers = data.interestedPlayers;
+			} );
+
+			server.create( game.gameData ).then( ( gameId ) => game.gameId = gameId );
+			element.appendChild( game.view.render() );
 			resolve( game );
 		} );
 	}
@@ -88,68 +112,39 @@ export default class Game {
 		const server = new Server();
 		let game;
 
-		return server.join( gameId ).then( ( response ) => {
-			if ( response.status == 'available' ) {
-				game = new Game( server, response.gameData.size, response.gameData.shipsConfig );
-				game.playerBattlefield.random();
-				element.appendChild( game.view.render() );
-			} else if ( response.status == 'started' ) {
+		return server.join( gameId ).then( ( data ) => {
+			if ( data.status == 'started' ) {
 				alert( 'Sorry this game has already started.' );
-			} else {
+			} else if ( data.status != 'available' ) {
 				alert( 'Sorry this game not exist.' );
+			} else {
+				game = new Game( server, data.gameData.size, data.gameData.shipsConfig );
+
+				game.player.battlefield.random();
+				game.player.isPresent = false;
+				game.opponent.isReady = data.opponentIsReady;
+				game.interestedPlayers = data.interestedPlayers;
+
+				game._attachEvents();
+				server.on( 'accepted', () => alert( 'Game is started.' ) );
+				server.on( 'left', ( event, data ) => game.interestedPlayers = data.interestedPlayers );
+
+				element.appendChild( game.view.render() );
 			}
 
 			return game;
 		} );
 	}
+
+	_attachEvents(  ) {
+		this._server.on( 'joined', ( event, data ) => this.interestedPlayers = data.interestedPlayers );
+		this._server.on( 'ready', () => this.opponent.isReady = true );
+		this._server.on( 'started', () => this.isStarted = true );
+		this._server.on( 'gameOver', () => alert( 'Game is over.' ) );
+	}
+
+	destroy() {
+	}
 }
 
 mix( Game, ObservableMixin );
-
-/**
- * Fired when the opponent leave the game or lost connection with the server.
- *
- * @event game#opponentJoin
- */
-
-/**
- * Fired when the opponent set ships on the battlefield and is ready to the battle.
- *
- * @event game#opponentLeave
- */
-
-/**
- * Fired when the opponent set ships on the battlefield and is ready to the battle.
- *
- * @event game#opponentReady
- */
-
-/**
- * Fired when turn is changing from the player to the opponent or from the opponent to the player.
- *
- * @event game#changeTurn
- */
-
-/**
- * Fired when the player win.
- *
- * @event game#win
- */
-
-/**
- * Fired when the player lost.
- *
- * @event game#lost
- */
-
-/**
- * Fired when the opponent wants rematch.
- *
- * @event game#rematchRequest
- */
-
-/**
- * Fired when the game is after reset for rematch.
- *
- * @event game#rematch
- */
