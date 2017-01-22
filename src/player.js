c620c731b8aecd206fd37c181f94925dbb178f78
Oskar Wróbel <kontakt@oskarwrobel.pl>
2017-01-22 20:56:55 +0100

    Improved code style.

--- src/player.js
+++ src/player.js
@@ -6,7 +6,9 @@ export default class Player {
 		this.battlefield = data.battlefield;
 
 		this.isHost = false;
+
 		this.set( 'isPresent', false );
+
 		this.set( 'isReady', false );
 
 		this.on( 'change:isReady', ( evt, name, value ) => this.battlefield.isLocked = value );
