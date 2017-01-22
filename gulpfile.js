26499352d29e4048822c82bc528ee41a45fb2921
Oskar Wróbel <kontakt@oskarwrobel.pl>
2017-01-22 02:09:46 +0100

    Changed uses of linting tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ const config = {
 	ROOT_PATH: __dirname
 };
 
-const path = require( 'path' );
 const gulp = require( 'gulp' );
 
 const utils = require( 'battleships-dev-tools/lib/utils.js' );
@@ -16,10 +15,7 @@ const options = utils.parseArgs( process.argv.slice( 3 ) );
 
 gulp.task( 'relink', relinkTasks.relink );
 
-// JS code sniffer.
-const jsFiles = [ path.join( config.ROOT_PATH, '**', '*.js' ) ];
-
-gulp.task( 'lint', () => lintTasks.lint( jsFiles ) );
-gulp.task( 'pre-commit', () => lintTasks.lintStaged( jsFiles ) );
+gulp.task( 'lint', lintTasks.lint );
+gulp.task( 'pre-commit', lintTasks.lintStaged );
 
 gulp.task( 'test', ( done ) => testTasks.test( options, done ) );
